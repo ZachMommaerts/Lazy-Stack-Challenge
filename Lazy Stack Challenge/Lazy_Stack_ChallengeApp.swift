53c120395ff4f08f632fb9537fc4f4e8ad0facf1
ZachMommaerts <zmommaerts@gmail.com>
2023-04-02 22:25:35 -0500

    Initial Commit

---
+++ Lazy Stack Challenge/Lazy_Stack_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lazy_Stack_ChallengeApp.swift
+//  Lazy Stack Challenge
+//
+//  Created by Zach Mommaerts on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Lazy_Stack_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
